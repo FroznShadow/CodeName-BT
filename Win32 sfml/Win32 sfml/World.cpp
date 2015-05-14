5b5793c10d6c94639180e622a6163dfba84512b2
Toni Lyytikäinen <dark.ace90@gmail.com>
2015-05-14 22:35:30 +0300

    world.cpp

--- Win32 sfml/Win32 sfml/World.cpp
+++ Win32 sfml/Win32 sfml/World.cpp
@@ -26,11 +26,12 @@ bool World::m_load(const std::string tileset, sf::Vector2u tileSize, const int* 
 		quad[2].position = sf::Vector2f((i + 1)*tileSize.x, (j + 1)*tileSize.y);
 		quad[3].position = sf::Vector2f(i*tileSize.x, (j + 1)*tileSize.y);
 
+
 		quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
 		quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
 		quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
 		quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
-		
+
 		}
 	return true;
 
